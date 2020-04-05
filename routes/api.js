40af21f36d0703daf6ce13a45db919c8f9001f39
Tyler <realpsych22@gmail.com>
2020-04-05 01:08:17 -0500

    drafted api.js

--- routes/api.js
+++ routes/api.js
@@ -19,12 +19,12 @@ var Schema = mongoose.Schema;
 
 var issueSchema = new Schema({
   issue_title: { type: String, required: true },
-  issue_text: String,
+  issue_text: { type: String, required: true },
   created_on: Date,
   updated_on: Date,
-  created_by: String,
+  created_by: { type: String, required: true },
   assigned_to: String,
-  open: Boolean,
+  open: { type: Boolean, default: true },
   status_text: String
 });
 
@@ -36,12 +36,16 @@ module.exports = function (app) {
   
     .get(function (req, res){
       var project = req.params.project;
+    if(!project.issue_title||!project.issue_text||!project.create_by){
+      return "please fill out required fields"
+    }
+    
     var createAndSaveIssue = function(done){
       var issue=new Issue({
   issue_title: project.issue_title,
   issue_text: project.issue_text,
-  created_on: project.created_on,
-  updated_on: project.updated_on,
+  created_on: Date.now(),
+  updated_on: Date.now(),
   created_by: project.created_by,
   assigned_to: project.assigned_to,
   open: project.open,
@@ -52,21 +56,58 @@ module.exports = function (app) {
     done(null,data);
   });
     }})
-
     
     .post(function (req, res){
       var project = req.params.project;
-      
+      var result ={
+      "_id": project._id,
+      "issue_title": project.issue_title,
+      "issue_text": project.issue_text,
+      "created_on": project.created_on,
+      "updated_on": project.updated_on,
+      "created_by": project.created_by,
+      "assigned_to": project.assigned_to,
+      "open": project.open,
+      "status_text": project.status_text}
+      res.json(result);
     })
     
     .put(function (req, res){
       var project = req.params.project;
-      
+      if(!project){res.send('no updated field sent')}
+      Issue.findOneAndUpdate({_id: project._id}, {new: true}, function(err, data){
+      if(err){res.send('could not update '+project._id)}
+      if(project.issue_title){
+        data.issue_title=project.issue_title;
+      }
+      if(project.issue_text){
+        data.issue_text=project.issue_text
+      }
+      if(project.created_by){
+        data.created_by=project.created_by;
+      }
+      if(project.assigned_to){
+        data.assigned_to=project.assigned_to;
+      }
+      if(project.open){
+        data.open=project.open;
+      }
+      if(project.status_text){
+        data.status_text=project.status_text;
+      }
+      data.update_date=Date.now();
+      data.save();
+      res.send('successfully updated')
+    });
     })
     
     .delete(function (req, res){
       var project = req.params.project;
-      
+    Issue.findByIdAndRemove({_id: project._id}, function(err, data){ 
+      if (!data){res.send('id error')}
+      if(err) res.send("could not delete "+project._id);
+       res.send(null, 'deleted '+data._id);
+    });
     });
     
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -29,23 +29,56 @@ suite('Functional Tests', function() {
         })
         .end(function(err, res){
           assert.equal(res.status, 200);
-          
-          //fill me in too!
-          
+          assert.equal(res.body,  {
+          issue_title: 'Title',
+          issue_text: 'text',
+          created_by: 'Functional Test - Every field filled in',
+          assigned_to: 'Chai and Mocha',
+          status_text: 'In QA'
+          })
           done();
         });
       });
       
       test('Required fields filled in', function(done) {
-        
+         chai.request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: 'Title',
+          issue_text: 'text',
+          created_by: 'Functional Test - Every field filled in',
+          assigned_to: 'Chai and Mocha',
+          status_text: 'In QA'
+        })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body,  {
+          issue_title: 'Title',
+          issue_text: 'text',
+          created_by: 'Functional Test - Every field filled in',
+          assigned_to: 'Chai and Mocha',
+          status_text: 'In QA'
+          })
+          done();
+        });      
       });
       
       test('Missing required fields', function(done) {
-        
+        chai.request(server)
+        .post('/api/issues/test')
+        .send({
+          issue_title: '',
+          issue_text: 'text',
+          created_by: 'Functional Test - Every field filled in',
+          assigned_to: 'Chai and Mocha',
+          status_text: 'In QA'
+        })
+        .end(function(err, res){  
+        assert.equal(res.status, 200);
+        assert.equal(res.body, "please fill out required fields")
       });
-      
     });
-    
+    });
     suite('PUT /api/issues/{project} => text', function() {
       
       test('No body', function(done) {

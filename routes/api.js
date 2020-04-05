c9049e2f2a499e1e544a84ffc4c63a0dcbbe761d
Tyler <realpsych22@gmail.com>
2020-04-04 20:36:16 -0500

    connect to MongoDB, set up schema

--- routes/api.js
+++ routes/api.js
@@ -11,8 +11,24 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 var ObjectId = require('mongodb').ObjectID;
+const mongoose = require("mongoose");
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+var Schema = mongoose.Schema;
+
+var issueSchema = new Schema({
+  issue_title: { type: String, required: true },
+  issue_text: String,
+  created_on: Date,
+  updated_on: Date,
+  created_by: String,
+  assigned_to: String,
+  open: Boolean,
+  status_text: String
+});
+
+var Issue = mongoose.model("Person", issueSchema);
 
 module.exports = function (app) {
 
